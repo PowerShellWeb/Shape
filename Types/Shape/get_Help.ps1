601e609d6f2dedf9e8f5c241c9158ad17be3a296
StartAutomating <james.brundage@start-automating.com>
2026-01-04 14:04:32 -0800

    feat: Shape.Help ( Fixes #7 )
    Alphabetizing

--- Types/Shape/get_Help.ps1
+++ Types/Shape/get_Help.ps1
@@ -1,11 +1,11 @@
 return [Ordered]@{
     'circle' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/circle'
     'ellipse' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/ellipse'
-    'polygon' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/polygon'
     'inset' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/inset'
-    'rect' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/rect'
-    'xywh' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/xywh'
     'path' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/path'
+    'polygon' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/polygon'
+    'rect' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/rect'
     'shape' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/shape'
+    'xywh' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/xywh'    
 }
 
