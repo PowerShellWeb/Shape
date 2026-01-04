c4f685e0e568c8026e14d254e94573536bf36418
StartAutomating <james.brundage@start-automating.com>
2026-01-03 22:30:50 -0800

    feat: Shape.Help ( Fixes #7 )

---
+++ Types/Shape/get_Help.ps1
@@ -0,0 +1,11 @@
+return [Ordered]@{
+    'circle' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/circle'
+    'ellipse' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/ellipse'
+    'polygon' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/polygon'
+    'inset' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/inset'
+    'rect' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/rect'
+    'xywh' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/xywh'
+    'path' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/path'
+    'shape' = 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/basic-shape/shape'
+}
+
