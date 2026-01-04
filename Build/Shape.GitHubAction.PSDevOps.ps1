831c52cc8876e3e211a6d783fd2bbf0e666df094
StartAutomating <james.brundage@start-automating.com>
2026-01-03 22:21:34 -0800

    feat: Shape Action ( Fixes #3 )

---
+++ Build/Shape.GitHubAction.PSDevOps.ps1
@@ -0,0 +1,10 @@
+﻿#requires -Module PSDevOps
+Import-BuildStep -SourcePath (
+    Join-Path $PSScriptRoot 'GitHub'
+) -BuildSystem GitHubAction
+
+$PSScriptRoot | Split-Path | Push-Location
+
+New-GitHubAction -Name "NewShape" -Description 'Shape Generator' -Action ShapeAction -Icon chevron-right -OutputPath .\action.yml
+
+Pop-Location
